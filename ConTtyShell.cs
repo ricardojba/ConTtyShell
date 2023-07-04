180eaa1152e676e14222d9b3caab8325966e24ba
Ricardo <ricardojba@users.noreply.github.com>
2023-07-04 15:38:35 +0100

    Update ConTtyShell.cs

--- ConTtyShell.cs
+++ ConTtyShell.cs
@@ -1,7 +1,3 @@
-
-
-// C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /unsafe /platform:x64 -nologo -debug -out:ConPtyShell.exe .\ConPtyShell.cs
-
 using System;
 using System.IO;
 using System.Text;
